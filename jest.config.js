eefd4be6d3f85163558987565a1e77019c7348a8
daniel <daniel@ci.ufpb.br>
2026-01-12 17:28:19 -0300

    test: enahnce jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,12 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     bail: 1,
-    verbose: true
+    verbose: true,
+    moduleNameMapper: {
+        '^axios$': require.resolve('axios/dist/node/axios.cjs'),
+        '^@services/(.*)$': '<rootDir>/src/services/$1',
+        '^@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+        '^@config/(.*)$': '<rootDir>/src/config/$1',
+        '^@models/(.*)$': '<rootDir>/src/models/$1'
+    }
 };
